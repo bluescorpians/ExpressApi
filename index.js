3399d9ba1db02385447b1785e2f90f63d4f1932b
SumitKumar Prasad <sumit-kumar-prasad-br19-2034@gcect.ac.in>
2023-08-03 18:24:23 +0530

    created REST API

--- index.js
+++ index.js
@@ -6,17 +6,56 @@ const server = express();
 const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
 const products = data.products;
 
+server.use(express.json());
+
 // create API
 
+server.post("/products", (req, res) => {
+  products.push(req.body);
+  res.send(200);
+});
 
+// read API
 server.get("/products", (req, res) => {
   res.json(products);
 });
 
+// get particular product with id
 
+server.get("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const ind = products.findIndex((p) => p.id === id);
+  res.json(products[ind]);
+});
 
+// update API
+
+server.put("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const ind = products.findIndex((p) => p.id === id);
+  products.splice(ind, 1, { ...req.body, id: id });
+  res.send(200);
+});
+
+// patch API
+
+server.patch("/products/:id", (req, res) => {
+  const id = +req.params.id;
+  const ind = products.findIndex((p) => p.id === id);
+  const temp = products[ind];
+  products.splice(ind, 1, { ...temp, ...req.body });
+  res.send(200);
+});
 server.get("/", (req, res) => {
   res.send("200");
 });
 
+// delete
+
+server.delete("/products/:id", (req, res) => {
+  const id = +  req.params.id;
+  const ind = products.findIndex((p) => p.id === id);
+  products.splice(ind, 1);
+  res.send(200);
+});
 server.listen(8080);
